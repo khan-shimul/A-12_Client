67e1a9aa60cf864e6f4573198e53ee6a77070fb0
emonkhan500 <khdilo47@gmail.com>
2024-06-08 23:48:26 +0600

    user profile created

--- src/Components/UserThing/UserProfile.jsx
+++ src/Components/UserThing/UserProfile.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../Provider/AuthPorvider';
 
 const UserProfile = () => {
+  const {user}=useContext(AuthContext)
     return (
       <div className="flex items-center flex-col justify-center pt-20 p-6 shadow-md rounded-xl sm:px-12 dark:bg-gray-50 dark:text-gray-800">
       <h1 className='pb-10 text-3xl font-bold text-yellow-500 '>Here is your all information what we have!!</h1>
@@ -12,9 +14,9 @@ const UserProfile = () => {
 <div className="space-y-4 text-center divide-y dark:divide-gray-300">
 <div className="my-2 space-y-1">
 <h2 className="text-2xl font-semibold sm:text-2xl"><span className='text-xl mr-1 text-orange-500'>Name:</span> {user?.displayName}</h2>
-<p className="px-5 text-2xl sm:text-base dark:text-gray-600 font-semibold"><span className='text-xl mr-1 text-orange-500'>Profession:</span> MERN-stack Developer </p>
+<p className="px-5 text-2xl sm:text-base dark:text-gray-600 font-semibold"><span className='text-xl mr-1 text-orange-500'>Profession:</span> Not Provided </p>
 <p className="px-5 text-2xl sm:text-base dark:text-gray-600 font-semibold"><span className='text-xl mr-1 text-orange-500'>Email:</span> {user?.email}</p>
-<p className="px-5 text-2xl sm:text-base dark:text-gray-600 font-semibold"><span className='text-xl mr-1 text-orange-500'>Role:</span> Admin!!</p>
+<p className="px-5 text-2xl sm:text-base dark:text-gray-600 font-semibold"><span className='text-xl mr-1 text-orange-500'>Role:</span> User</p>
 </div>
 
 
