dcded23cc3ebd7bb57163643909431990ef290c4
emonkhan500 <khdilo47@gmail.com>
2024-06-27 11:14:24 +0600

    modified 1

--- src/Components/ShowAdvertise/ShowAdvertise.jsx
+++ src/Components/ShowAdvertise/ShowAdvertise.jsx
@@ -22,7 +22,7 @@ const ShowAdvertise = () => {
         <div 
         
         className='bg-gray-200 my-10 rounded-lg'>
-            <div className='text-center py-10 '> <h1 className='text-3xl font-bold text-orange-400 mt-5 animate__animated animate__backInDown'>Our Best Property In Here!!</h1></div>
+            <div className='text-center py-10 '> <h1 className='text-3xl font-bold text-orange-400 mt-5 animate__animated animate__backInDown'>Our Best Property is Here!!</h1></div>
             <div className='mt-7 pb-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-5 gap-4'>
                 {
                     advertiseProperties?.map(property=><ShowUiAdvertisement property={property}></ShowUiAdvertisement>)
